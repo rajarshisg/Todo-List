34b04c18a4e37b8a3d8944564acc37b0cbc658de
rajarshisg <rajarshisengupta50@gmail.com>
2021-06-06 19:44:10 +0530

    Setting up the directory structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express'); //requiring express
+const port = 8000; //port on which our server runs
+
+const path = require('path'); //requires the path library, needed for path.join()
+
+const app = express(); //app has all the functionalities of express
+
+app.set('view engine', 'ejs'); //setting up the view engine to EJS
+app.set('views', path.join(__dirname,'views')); //providing the path for our views
+
+//server listens for requests on port 8000
+app.listen(port, function(err){
+    if(err){
+        //if any error occures on firing up the server
+        console.log(`Error ${err} occured in running the server!`);
+        return;
+    }
+    //if no error occured
+    console.log(`Server is up and running on port ${port}!`);
+});
